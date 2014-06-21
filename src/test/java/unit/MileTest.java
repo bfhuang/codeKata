5591f81f54a03f8cb7a46ff28dcb7f021ac0d8a5
bfhuang <bfhuang@thoughtworks.com>
2014-06-21 18:25:40 +0800

    3mile should equals 3 mile

--- src/test/java/unit/MileTest.java
+++ src/test/java/unit/MileTest.java
@@ -3,17 +3,20 @@ package unit;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MileTest {
 
-    private Mile mile;
 
     @Test
     public void should_3_mile_equals_3_mile() {
-        mile = new Mile(3);
+        assertThat(new Mile(3), is(new Mile(3)));
+    }
 
-        assertThat(mile, is(new Mile(3)));
+    @Test
+    public void should_3_mile_not_equals_4_miles() {
+        assertThat(new Mile(3), not(new Mile(4)));
     }
 
 
